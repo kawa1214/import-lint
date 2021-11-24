c833a5803bdf8ec1c55453993b34b2ae0b8d3055
ryo kawamura <kawa1214mura@gmail.com>
2021-11-25 02:50:21 +0900

    add ignore: generated files

--- test/helper/generated_project/lib/custom/first_target.dart
+++
@@ -1,6 +0,0 @@
-import 'package:example/custom/first_target.dart';
-import 'package:example/custom/first_allow.dart';
-import 'package:example/custom/second_allow.dart';
-import 'package:example/custom/first_not_allow.dart';
-import 'package:example/custom/second_not_allow.dart';
-import 'package:example/custom/exclude.dart';
