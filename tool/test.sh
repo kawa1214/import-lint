a6a27c4bc716a32dd509844c9a2a1c8381449451
ryo kawamura <kawa1214mura@gmail.com>
2021-11-26 22:23:31 +0900

    add: workflows

--- tool/test.sh
+++ tool/test.sh
@@ -8,7 +8,7 @@ sleep 1
 
 dart run test test/run.dart --chain-stack-traces -t presubmit-only --run-skipped --coverage=coverage || EXIT_CODE=$?;
 
-format_coverage --packages=.import_lint_packages -i coverage/test/run.dart.vm.json -o coverage/lcov/coverage.info -l
+dart run coverage:format_coverage --packages=.import_lint_packages -i coverage/test/run.dart.vm.json -o coverage/lcov/coverage.info -l
 genhtml coverage/lcov/coverage.info -o coverage/html -q
 
 sleep 1
