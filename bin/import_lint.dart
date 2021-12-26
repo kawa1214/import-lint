06839348bfcffb5f6448e001b66ec938c20eb4a9
ryo kawamura <kawa1214mura@gmail.com>
2021-12-26 16:21:00 +0900

    lib export

--- bin/import_lint.dart
+++ bin/import_lint.dart
@@ -1,19 +1,20 @@
 import 'dart:io' as io;
 
-import 'package:import_lint/src/import_lint_analyze.dart';
-import 'package:import_lint/src/import_lint_options.dart';
+import 'package:import_lint/import_lint.dart';
 import 'package:cli_util/cli_logging.dart';
 
 void main(List<String> arguments) async {
   try {
     final logger = Logger.standard();
-
     final progress = logger.progress('Analyzing');
 
     final rootDirectoryPath = io.Directory.current.path;
     final options = ImportLintOptions.init(directoryPath: rootDirectoryPath);
+
     final analyzed = await ImportLintAnalyze.ofInitCli(options: options);
+
     progress.finish(showTiming: true);
+
     logger.stdout('');
     logger.stdout(analyzed.output);
 
---
+++ lib/import_lint.dart
@@ -0,0 +1,2 @@
+export 'package:import_lint/src/import_lint_analyze.dart';
+export 'package:import_lint/src/import_lint_options.dart';
