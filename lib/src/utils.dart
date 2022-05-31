180aa6d94cd386e86061a3e4f0dffd0f9361a635
Ryo Kawamura <kawa1214mura@gmail.com>
2022-06-01 00:12:22 +0900

    rm: toProjectPath

--- lib/src/utils.dart
+++ lib/src/utils.dart
@@ -1,19 +1,4 @@
 import 'package:analyzer/file_system/physical_file_system.dart';
-import 'package:import_lint/import_lint.dart';
-
-String toProjectPath({
-  required String path,
-  required LintOptions options,
-}) {
-  final fixedPath = path.replaceFirst('${options.common.directoryPath}', '');
-  if (fixedPath.startsWith('/lib/')) {
-    return fixedPath.replaceFirst('/lib/', '');
-  }
-  if (fixedPath.startsWith(r'\lib\')) {
-    return fixedPath.replaceFirst(r'\lib\', '');
-  }
-  return fixedPath;
-}
 
 String toPackagePath(
   String path,
