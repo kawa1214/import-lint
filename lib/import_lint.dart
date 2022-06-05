04e7170327f3be4e0b34aeaa5b05e4ff9cdb8625
Ryo Kawamura <kawa1214mura@gmail.com>
2022-06-06 02:18:13 +0900

    export

--- lib/import_lint.dart
+++ lib/import_lint.dart
@@ -1,2 +1,4 @@
 export 'package:import_lint/src/cli.dart';
 export 'package:import_lint/src/lint_options.dart';
+export 'package:import_lint/src/rule.dart';
+export 'package:import_lint/src/utils.dart';
