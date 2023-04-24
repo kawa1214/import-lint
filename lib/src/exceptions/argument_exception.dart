70794e9407ccc4e8e6ce7e85489cc1e866b03acd
Ryo Kawamura <kawa1214mura@gmail.com>
2023-04-25 00:56:53 +0900

    refactoring: base exception

---
+++ lib/src/exceptions/argument_exception.dart
@@ -0,0 +1,5 @@
+import 'base_exception.dart';
+
+class ArgumentException extends BaseException {
+  const ArgumentException(String message) : super(message);
+}
---
+++ lib/src/exceptions/base_exception.dart
@@ -0,0 +1,4 @@
+abstract class BaseException implements Exception {
+  const BaseException(this.message);
+  final String message;
+}
