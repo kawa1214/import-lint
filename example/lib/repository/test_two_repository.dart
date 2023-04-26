505cf89af7516f65e817db8438211744f8a67c99
Ryo Kawamura <kawa1214mura@gmail.com>
2023-04-27 00:43:12 +0900

    update: example

--- example/lib/repository/test_two_repository.dart
+++
@@ -1,5 +0,0 @@
-import 'package:example/use_case/test_one_use_case.dart';
-
-class TestTwoRepository {}
-
-final useCase = TestOneUseCase();
--- example/lib/use_case/foo_use_case.dart
+++
@@ -1,7 +0,0 @@
-import 'package:example/repository/test_one_repository.dart';
-import 'package:example/use_case/test_one_use_case.dart';
-
-class FooUseCase {}
-
-final tesOneUseCase = TestOneUseCase();
-final testTwoRepository = TestOneRepository();
