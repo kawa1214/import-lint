6b4554c47a673a4c6696004fa9ef53006ed9dbfd
Ryo Kawamura <kawa1214mura@gmail.com>
2023-04-27 03:33:33 +0900

    feat: Parse SourcePath from ImportDirective

--- lib/src/analyzer/path.dart
+++ lib/src/analyzer/path.dart
@@ -1,25 +1,60 @@
 import 'package:analyzer/dart/analysis/results.dart' show ResolvedUnitResult;
+import 'package:analyzer/dart/ast/ast.dart' show ImportDirective;
 import 'package:analyzer/src/dart/analysis/driver_based_analysis_context.dart'
     show DriverBasedAnalysisContext;
+import 'package:analyzer/src/dart/element/element.dart'
+    show DirectiveUriWithLibraryImpl, DirectiveUriWithRelativeUriImpl;
 import 'package:analyzer/src/workspace/pub.dart' show PubWorkspacePackage;
 import 'package:import_lint/src/exceptions/argument_exception.dart';
 import 'package:import_lint/src/exceptions/internal_exception.dart';
 
-class SourcePath extends Path {
+class SourcePath implements Path {
   const SourcePath({
     required this.package,
     required this.path,
-  }) : super(package: package, path: path);
+  });
+
+  factory SourcePath.fromImportDirective(
+    ImportDirective directive,
+    FilePath filePath,
+  ) {
+    final uri = directive.element?.uri;
+
+    if (uri is DirectiveUriWithLibraryImpl) {
+      // uri is import '../../import_lint.dart';
+      final fullUri = uri.source.fullName;
+      final path = RegExp('\/lib\/(.*)').firstMatch(fullUri)?.group(1);
+      if (path == null) {
+        throw InternalException('path is null');
+      }
+
+      return SourcePath(package: filePath.package, path: path);
+    } else if (uri is DirectiveUriWithRelativeUriImpl) {
+      // uri is import 'package:import_lint/import_lint.dart';
+      final relativeUri = uri.relativeUriString;
+
+      final package = RegExp('(?<=package:).*?(?=\/)').stringMatch(relativeUri);
+      if (package == null) {
+        throw InternalException('package is null');
+      }
+
+      final path = relativeUri.replaceFirst('package:$package/', '');
+
+      return SourcePath(package: package, path: path);
+    }
+
+    throw InternalException('uri is not DirectiveUriWithLibraryImpl');
+  }
 
   final String package;
   final String path;
 }
 
-class FilePath extends Path {
+class FilePath implements Path {
   const FilePath({
     required this.package,
     required this.path,
-  }) : super(package: package, path: path);
+  });
 
   factory FilePath.fromResolvedUnitResult(
     DriverBasedAnalysisContext context,
--- test/analyzer/path.test.dart
+++ test/analyzer/path.test.dart
@@ -1,4 +1,5 @@
 import 'package:analyzer/dart/analysis/results.dart' show ResolvedUnitResult;
+import 'package:analyzer/dart/ast/ast.dart' show ImportDirective;
 import 'package:import_lint/src/analyzer/path.dart';
 import 'package:test/expect.dart';
 import 'package:test_reflective_loader/test_reflective_loader.dart';
@@ -17,10 +18,8 @@ class PathTest with BaseResourceProviderMixin {
     setUp();
   }
 
-  void test_correctFormat() async {
+  void test_filePath() async {
     newFile('/lib/target/test.dart', '''
-void main() {
-}
 ''');
 
     final context = buildContext();
@@ -31,5 +30,65 @@ void main() {
 
     expect(path.package, packageName);
     expect(path.path, 'target/test.dart');
+  }
+
+  void test_relativeUriSourcePath() async {
+    newFile('/lib/target/test.dart', '''
+import 'package:example/from/test.dart';
+''');
+
+    final context = buildContext();
+    final result = await context.currentSession
+        .getResolvedUnit('/lib/target/test.dart') as ResolvedUnitResult;
+
+    final filePath = FilePath.fromResolvedUnitResult(context, result);
+
+    final directives = result.unit.directives;
+    final directive = directives[0] as ImportDirective;
+
+    final sourcePath = SourcePath.fromImportDirective(directive, filePath);
+
+    expect(sourcePath.package, packageName);
+    expect(sourcePath.path, 'from/test.dart');
+  }
+
+  void test_directiveUriSourcePath() async {
+    newFile('/lib/target/test.dart', '''
+import '../from/test.dart';
+''');
+
+    final context = buildContext();
+    final result = await context.currentSession
+        .getResolvedUnit('/lib/target/test.dart') as ResolvedUnitResult;
+
+    final filePath = FilePath.fromResolvedUnitResult(context, result);
+
+    final directives = result.unit.directives;
+    final directive = directives[0] as ImportDirective;
+
+    final sourcePath = SourcePath.fromImportDirective(directive, filePath);
+
+    expect(sourcePath.package, packageName);
+    expect(sourcePath.path, 'from/test.dart');
+  }
+
+  void test_anotherPackageSourcePath() async {
+    newFile('/lib/target/test.dart', '''
+import 'package:$anotherPackageName/src/example.dart';
+''');
+
+    final context = buildContext();
+    final result = await context.currentSession
+        .getResolvedUnit('/lib/target/test.dart') as ResolvedUnitResult;
+
+    final filePath = FilePath.fromResolvedUnitResult(context, result);
+
+    final directives = result.unit.directives;
+    final directive = directives[0] as ImportDirective;
+
+    final sourcePath = SourcePath.fromImportDirective(directive, filePath);
+
+    expect(sourcePath.package, anotherPackageName);
+    expect(sourcePath.path, 'src/example.dart');
   }
 }
