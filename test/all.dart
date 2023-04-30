454d961a3716d38a613b6db8006753392686e6ae
Ryo Kawamura <kawa1214mura@gmail.com>
2023-05-01 01:12:14 +0900

    chore: Change test target

--- test/all.dart
+++ test/all.dart
@@ -1,12 +1,27 @@
 @Tags(['presubmit-only'])
 import 'package:test/test.dart';
 
-import 'lint_options.test.dart' as lint_options;
-import 'rule.test.dart' as rule;
+import 'analyzer/constraint_resolver.test.dart' as constraint_resolver;
+import 'analyzer/resource_locator.test.dart' as resource_locator;
+import 'analyzer/visitor.test.dart' as visitor;
+import 'config/analysis_options.test.dart' as analysis_options;
+import 'config/config.test.dart' as config;
+import 'config/constraint.test.dart' as constraint;
+import 'config/rule.test.dart' as rule;
+import 'config/severity.test.dart' as severity;
 
 export 'helper/coverage_test.dart';
 
 void main() {
-  lint_options.main();
+  // config
+  analysis_options.main();
+  severity.main();
+  constraint.main();
   rule.main();
+  config.main();
+
+  // analyzer
+  resource_locator.main();
+  constraint_resolver.main();
+  visitor.main();
 }
