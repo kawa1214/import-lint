a8d04ae7eff21aed8498610f87866ef16805141d
Ryo Kawamura <kawa1214mura@gmail.com>
2023-05-02 04:19:01 +0900

    chore: fix test script

--- tool/test.sh
+++ tool/test.sh
@@ -6,11 +6,13 @@ bash tool/helper/dart_coverage_helper.sh
 
 sleep 1
 
-dart run test test/all.dart --chain-stack-traces -t presubmit-only --run-skipped --coverage=coverage || EXIT_CODE=$?;
+dart run test test/all.dart --chain-stack-traces --run-skipped --coverage=coverage || EXIT_CODE=$?;
 
-# dart run coverage:format_coverage -i coverage/test/all.dart.vm.json -o coverage/lcov/coverage.info -l --report-on=lib
+dart run coverage:format_coverage  --check-ignore -i coverage/test/all.dart.vm.json -o coverage/lcov/coverage.info -l --report-on=lib
 
-# genhtml coverage/lcov/coverage.info -o coverage/html -q
+lcov --remove coverage/lcov/coverage.info "**/lib/src/plugin/*" -o coverage/lcov/coverage.info
+
+genhtml coverage/lcov/coverage.info -o coverage/html -q
 
 sleep 1
 
