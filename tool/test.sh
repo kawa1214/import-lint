812c47befff5f53640d54f1a1301461cade9dbf4
Ryo Kawamura <kawa1214mura@gmail.com>
2023-05-02 22:33:46 +0900

    fix: github workflow

--- tool/test.sh
+++ tool/test.sh
@@ -10,7 +10,7 @@ dart run test test/all.dart --chain-stack-traces --run-skipped --coverage=covera
 
 dart run coverage:format_coverage  --check-ignore -i coverage/test/all.dart.vm.json -o coverage/lcov/coverage.info -l --report-on=lib
 
-# lcov --remove coverage/lcov/coverage.info "**/lib/src/plugin/*" -o coverage/lcov/coverage.info
+lcov --remove coverage/lcov/coverage.info "**/lib/src/plugin/*" -o coverage/lcov/coverage.info
 
 genhtml coverage/lcov/coverage.info -o coverage/html -q
 
