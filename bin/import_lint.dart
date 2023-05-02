e2bd717acfe5294a72ccb608a25c2e1a2795a9d1
Ryo Kawamura <kawa1214mura@gmail.com>
2023-05-02 18:06:20 +0900

    fix: exit to be done in bin

--- bin/import_lint.dart
+++ bin/import_lint.dart
@@ -1,8 +1,27 @@
+import 'dart:io';
+
+import 'package:analyzer/file_system/physical_file_system.dart'
+    show PhysicalResourceProvider;
+import 'package:analyzer/src/dart/analysis/analysis_context_collection.dart'
+    show AnalysisContextCollectionImpl;
 import 'package:cli_util/cli_logging.dart';
 import 'package:import_lint/src/cli/runner.dart';
 
 void main(List<String> args) async {
+  final instance = PhysicalResourceProvider.INSTANCE;
+
+  final collection = AnalysisContextCollectionImpl(
+    resourceProvider: instance,
+    includedPaths: [
+      instance.pathContext.normalize(
+        instance.pathContext.absolute('./lib'),
+      ),
+    ],
+  );
+  final context = collection.contexts.take(1).first;
+
   final logger = Logger.standard();
-  final runner = Runner(logger);
-  runner.run(args);
+  final runner = Runner(logger, context);
+  final exitCode = await runner.run(args);
+  exit(exitCode);
 }
