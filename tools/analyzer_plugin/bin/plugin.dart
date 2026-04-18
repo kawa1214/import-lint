4f7de5ed2b7a61cdb04b6225884727fa3144c0e3
Ryo Kawamura <ryo@kawa.dev>
2026-04-19 03:12:35 +0900

    chore: remove obsolete tools/analyzer_plugin bootstrap package

--- tools/analyzer_plugin/bin/plugin.dart
+++
@@ -1,7 +0,0 @@
-import 'dart:isolate';
-
-import 'package:import_lint/src/plugin/plugin_starter.dart' as plugin;
-
-void main(List<String> args, SendPort sendPort) {
-  plugin.start(args, sendPort);
-}
